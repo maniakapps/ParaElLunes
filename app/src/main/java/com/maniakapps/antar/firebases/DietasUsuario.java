fe02739382e9bca4c45fa353b56c25efcf7df4ff
antar <ti15110264@itess.edu.mx>
2018-09-29 18:36:33 -0500

    Se muetran correctamente la lista y sus valores

--- app/src/main/java/com/maniakapps/antar/firebases/DietasUsuario.java
+++ app/src/main/java/com/maniakapps/antar/firebases/DietasUsuario.java
@@ -1,13 +1,18 @@
 package com.maniakapps.antar.firebases;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -24,17 +29,17 @@ import butterknife.ButterKnife;
 public class DietasUsuario extends AppCompatActivity {
     @BindView(R.id.listViewUser)
     ListView listViewUser;
-    private ArrayList<Dietas> list;
-    ArrayAdapter<Dietas> adapter;
-    Dietas dies;
-
+    private ArrayList<String> list;
+    private ArrayList<String> listTextos;
+    ArrayAdapter<String> adapter;
+    String options;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usuario);
         ButterKnife.bind(this);
-        dies = new Dietas();
         getDatas();
+
     }
 
     private void getDatas() {
@@ -43,16 +48,26 @@ public class DietasUsuario extends AppCompatActivity {
         database = FirebaseDatabase.getInstance();
         ref = database.getReference("dietas");
         list = new ArrayList<>();
+        listTextos = new ArrayList<>();
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
         listViewUser.setAdapter(adapter);
         ChildEventListener childEventListener = new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                Dietas value = dataSnapshot.getValue(Dietas.class);
-                value.setTitulo(dataSnapshot.getKey());
-                list.add(value);
-                Log.d("HelloPwr",value.getTexto()+value.getTitulo());
+                final Dietas value = dataSnapshot.getValue(Dietas.class);
+                assert value != null;
+                list.add(value.getTitulo());
+                listTextos.add(value.getTexto());
                 adapter.notifyDataSetChanged();
+                listViewUser.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view,
+                                            int position, long id) {
+                        displayToast("Click ListItem Number " + position);
+                        displayToast(listTextos.get(position));
+
+                    }
+                });
             }
 
             @Override
@@ -76,8 +91,15 @@ public class DietasUsuario extends AppCompatActivity {
             }
         };
         ref.addChildEventListener(childEventListener);
+
+
     }
+
     public void onBackPressed(){
         startActivity(new Intent(this,DietasAdmin.class));
     }
+    private void displayToast(String a){
+        Toast.makeText(getApplicationContext(),a,Toast.LENGTH_LONG).show();
+    }
+
 }
